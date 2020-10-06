47be40879a7dc54a2ad48015f73418a26e7a2bc4
FrederikWarburg <frewar1905@gmail.com>
2020-10-06 17:49:23 +0100

    multiple depth src inputs

--- src/summary/nlspnsummary.py
+++ src/summary/nlspnsummary.py
@@ -127,8 +127,8 @@ class NLSPNSummary(BaseSummary):
         if self.args.dep_src in ['slam', 'sgbm']:
             dep = np.clip(dep, a_min=0, a_max=self.args.max_depth)
         else:
-            dep0 = np.clip(dep, a_min=0, a_max=self.args.max_depth)
-            dep1 = np.clip(dep, a_min=0, a_max=self.args.max_depth)
+            dep0 = np.clip(dep0, a_min=0, a_max=self.args.max_depth)
+            dep1 = np.clip(dep1, a_min=0, a_max=self.args.max_depth)
         gt = np.clip(gt, a_min=0, a_max=self.args.max_depth)
         pred = np.clip(pred, a_min=0, a_max=self.args.max_depth)
         confidence = np.clip(confidence, a_min=0, a_max=1.0)
@@ -223,7 +223,11 @@ class NLSPNSummary(BaseSummary):
                 list_feat = output['pred_inter']
 
                 rgb = sample['rgb'].detach()
-                dep = sample['dep'].detach()
+                if self.args.dep_src in ['slam', 'sgbm']:
+                    dep = sample['dep'].detach()
+                else:
+                    dep0 = sample['dep0'].detach()
+                    dep1 = sample['dep1'].detach()
                 pred = output['pred'].detach()
                 gt = sample['gt'].detach()
 
@@ -234,24 +238,40 @@ class NLSPNSummary(BaseSummary):
                     self.img_mean.type_as(rgb))
 
                 rgb = rgb[0, :, :, :].data.cpu().numpy()
-                dep = dep[0, 0, :, :].data.cpu().numpy()
+                if self.args.dep_src in ['slam', 'sgbm']:
+                    dep = dep[0, 0, :, :].data.cpu().numpy()
+                else:
+                    dep0 = dep0[0, 0, :, :].data.cpu().numpy()
+                    dep1 = dep1[0, 0, :, :].data.cpu().numpy()
                 pred = pred[0, 0, :, :].data.cpu().numpy()
                 gt = gt[0, 0, :, :].data.cpu().numpy()
 
                 rgb = 255.0*np.transpose(rgb, (1, 2, 0))
-                dep = dep / self.args.max_depth
+                if self.args.dep_src in ['slam', 'sgbm']:
+                    dep = dep / self.args.max_depth
+                else:
+                    dep0 = dep0 / self.args.max_depth
+                    dep1 = dep1 / self.args.max_depth
                 pred = pred / self.args.max_depth
                 pred_gray = pred
                 gt = gt / self.args.max_depth
 
                 rgb = np.clip(rgb, 0, 256).astype('uint8')
-                dep = (255.0*cm(dep)).astype('uint8')
+                if self.args.dep_src in ['slam', 'sgbm']:
+                    dep = (255.0*cm(dep)).astype('uint8')
+                else:
+                    dep0 = (255.0*cm(dep0)).astype('uint8')
+                    dep1 = (255.0*cm(dep1)).astype('uint8')
                 pred = (255.0*cm(pred)).astype('uint8')
                 pred_gray = (255.0*pred_gray).astype('uint8')
                 gt = (255.0*cm(gt)).astype('uint8')
 
                 rgb = Image.fromarray(rgb, 'RGB')
-                dep = Image.fromarray(dep[:, :, :3], 'RGB')
+                if self.args.dep_src in ['slam', 'sgbm']:
+                    dep = Image.fromarray(dep[:, :, :3], 'RGB')
+                else:
+                    dep0 = Image.fromarray(dep0[:, :, :3], 'RGB')
+                    dep1 = Image.fromarray(dep1[:, :, :3], 'RGB')
                 pred = Image.fromarray(pred[:, :, :3], 'RGB')
                 pred_gray = Image.fromarray(pred_gray)
                 gt = Image.fromarray(gt[:, :, :3], 'RGB')
@@ -275,7 +295,11 @@ class NLSPNSummary(BaseSummary):
                 os.makedirs(self.path_output, exist_ok=True)
 
                 path_save_rgb = '{}/01_rgb.png'.format(self.path_output)
-                path_save_dep = '{}/02_dep.png'.format(self.path_output)
+                if self.args.dep_src in ['slam', 'sgbm']:
+                    path_save_dep = '{}/02_dep.png'.format(self.path_output)
+                else:
+                    path_save_dep1 = '{}/02_dep1.png'.format(self.path_output)
+                    path_save_dep2 = '{}/02_dep2.png'.format(self.path_output)
                 path_save_init = '{}/03_pred_init.png'.format(self.path_output)
                 path_save_pred = '{}/05_pred_final.png'.format(self.path_output)
                 path_save_pred_gray = '{}/05_pred_final_gray.png'.format(
@@ -283,7 +307,11 @@ class NLSPNSummary(BaseSummary):
                 path_save_gt = '{}/06_gt.png'.format(self.path_output)
 
                 rgb.save(path_save_rgb)
-                dep.save(path_save_dep)
+                if self.args.dep_src in ['slam', 'sgbm']:
+                    dep.save(path_save_dep)
+                else:
+                    dep0.save(path_save_dep0)
+                    dep1.save(path_save_dep1)
                 pred.save(path_save_pred)
                 pred_gray.save(path_save_pred_gray)
                 feat_init.save(path_save_init)
