d35e2f03cdb95d27feef1ec754e82be1fa1ac6c9
FrederikWarburg <frewar1905@gmail.com>
2020-10-12 17:14:19 +0100

    update

--- src/model/nconv.py
+++ src/model/nconv.py
@@ -7,6 +7,7 @@ __email__ = "abdo.eldesokey@gmail.com"
 ########################################
 
 import torch
+import torch.nn as nn
 import torch.nn.functional as F
 from torch.nn.parameter import Parameter
 from torch.nn.modules.conv import _ConvNd
