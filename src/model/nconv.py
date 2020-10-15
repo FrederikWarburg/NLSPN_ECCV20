0ba75df66f47973c017903db557c0c6d40afb874
FrederikWarburg <frewar1905@gmail.com>
2020-10-15 14:25:26 +0100

    understand shape of nconv

--- src/model/nconv.py
+++ src/model/nconv.py
@@ -108,16 +108,13 @@ class NConv2d(_ConvNd):
             EnforcePos.apply(self, 'weight', pos_fn)
         
     def forward(self, data, conf):
-        print()
-        print(data.shape, conf.shape)
+
         # Normalized Convolution
         denom = F.conv2d(conf, self.weight, None, self.stride,
                         self.padding, self.dilation, self.groups)        
         nomin = F.conv2d(data*conf, self.weight, None, self.stride,
                         self.padding, self.dilation, self.groups)        
         nconv = nomin / (denom+self.eps)
-
-        print(nconv.shape, nomin.shape, denom.shape)
 
         # Add bias
         b = self.bias
@@ -139,9 +136,6 @@ class NConv2d(_ConvNd):
         cout = cout / s
         cout = cout.view(sz)
         
-        print(cout.shape, k.shape)
-        print()
-        #exit()
         return nconv, cout
     
     def enforce_pos(self):
