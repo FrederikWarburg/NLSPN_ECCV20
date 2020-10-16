616142a7b9c3c0696b828de39f47a758a6375358
FrederikWarburg <frewar1905@gmail.com>
2020-10-16 11:15:41 +0100

    benchmark models

---
+++ src/scripts/tartanair_benchmarks.sh
@@ -0,0 +1,190 @@
+#/bin/bash
+
+
+###############
+# GENERAL
+###############
+
+DATAPATH="/media/slamcore/frederik/TartanairIndoorReduced"
+DATANAME="TARTANAIR"
+JSONPATH="../data_json/tartanair_reduced.json"
+
+DEPSRC="slam"
+MAXDEPTH=15.0
+NUM_SAMPLE=0
+
+LEFTCROP=110
+TOPCROP=0
+PATCH_HEIGHT=65
+PATHC_WIDTH=300
+
+###############
+# NLSPN
+###############
+
+LOSS="1.0*L1+1.0*L2"
+EPOCHS=60
+BATCH_SIZE=50
+SAVE="NLSPN_TartanairReduced"
+MODELNAME='NLSPN'
+INPUTCONF='input'
+DEVICE="0"
+PORT=30001
+
+python main.py --dir_data $DATAPATH \
+                --data_name $DATANAME \ 
+                --split_json $JSONPATH \ 
+                --patch_height $PATCH_HEIGHT \ 
+                --patch_width $PATHC_WIDTH \
+                --gpus $DEVICE \ 
+                --loss $LOSS \ 
+                --epochs $EPOCHS \
+                --batch_size $BATCH_SIZE \ 
+                --max_depth $MAXDEPTH \ 
+                --num_sample $NUM_SAMPLE \ 
+                --top_crop $TOPCROP \ 
+                --left_crop $LEFTCROP
+                --test_crop \ 
+                --save $SAVE \ 
+                --dep_src $DEPSRC \ 
+                --augment True \ 
+                --model_name $MODELNAME \ 
+                --port $PORT \  
+                --input_conf $INPUTCONF
+
+###############
+# NCONV ENCDEC
+###############
+
+LOSS="1.0*L2"
+EPOCHS=60
+BATCH_SIZE=50
+SAVE="NCONV_ENCDEC_TartanairReduced"
+MODELNAME='NCONV_ENCDEC'
+INPUTCONF='input'
+DEVICE="0"
+PORT=30001
+
+python main.py --dir_data $DATAPATH \
+                --data_name $DATANAME \ 
+                --split_json $JSONPATH \ 
+                --patch_height $PATCH_HEIGHT \ 
+                --patch_width $PATHC_WIDTH \
+                --gpus $DEVICE \ 
+                --loss $LOSS \ 
+                --epochs $EPOCHS \
+                --batch_size $BATCH_SIZE \ 
+                --max_depth $MAXDEPTH \ 
+                --num_sample $NUM_SAMPLE \ 
+                --top_crop $TOPCROP \ 
+                --left_crop $LEFTCROP
+                --test_crop \ 
+                --save $SAVE \ 
+                --dep_src $DEPSRC \ 
+                --augment True \ 
+                --model_name $MODELNAME \ 
+                --port $PORT \  
+                --input_conf $INPUTCONF
+
+###############
+# NCONV STREAM
+###############
+
+LOSS="1.0*L2"
+EPOCHS=60
+BATCH_SIZE=50
+SAVE="NCONV_STREAM_TartanairReduced"
+MODELNAME='NCONV_STREAM'
+INPUTCONF='input'
+DEVICE="0"
+PORT=30001
+
+python main.py --dir_data $DATAPATH \
+                --data_name $DATANAME \ 
+                --split_json $JSONPATH \ 
+                --patch_height $PATCH_HEIGHT \ 
+                --patch_width $PATHC_WIDTH \
+                --gpus $DEVICE \ 
+                --loss $LOSS \ 
+                --epochs $EPOCHS \
+                --batch_size $BATCH_SIZE \ 
+                --max_depth $MAXDEPTH \ 
+                --num_sample $NUM_SAMPLE \ 
+                --top_crop $TOPCROP \ 
+                --left_crop $LEFTCROP
+                --test_crop \ 
+                --save $SAVE \ 
+                --dep_src $DEPSRC \ 
+                --augment True \ 
+                --model_name $MODELNAME \ 
+                --port $PORT \  
+                --input_conf $INPUTCONF
+
+###############
+# NCONV UNGUIDED
+###############
+
+LOSS="1.0*L2"
+EPOCHS=60
+BATCH_SIZE=50
+SAVE="NCONV_UNGUIDED_TartanairReduced"
+MODELNAME='NCONV_UNGUIDED'
+INPUTCONF='input'
+DEVICE="0"
+PORT=30001
+
+python main.py --dir_data $DATAPATH \
+                --data_name $DATANAME \ 
+                --split_json $JSONPATH \ 
+                --patch_height $PATCH_HEIGHT \ 
+                --patch_width $PATHC_WIDTH \
+                --gpus $DEVICE \ 
+                --loss $LOSS \ 
+                --epochs $EPOCHS \
+                --batch_size $BATCH_SIZE \ 
+                --max_depth $MAXDEPTH \ 
+                --num_sample $NUM_SAMPLE \ 
+                --top_crop $TOPCROP \ 
+                --left_crop $LEFTCROP
+                --test_crop \ 
+                --save $SAVE \ 
+                --dep_src $DEPSRC \ 
+                --augment True \ 
+                --model_name $MODELNAME \ 
+                --port $PORT \  
+                --input_conf $INPUTCONF
+
+
+###############
+# PNCONV UNGUIDED
+###############
+
+LOSS="1.0*ProbExp"
+EPOCHS=60
+BATCH_SIZE=50
+SAVE="PNCONV_UNGUIDED_TartanairReduced"
+MODELNAME='PNCONV_UNGUIDED'
+INPUTCONF='learned'
+DEVICE="0"
+PORT=30001
+
+python main.py --dir_data $DATAPATH \
+                --data_name $DATANAME \ 
+                --split_json $JSONPATH \ 
+                --patch_height $PATCH_HEIGHT \ 
+                --patch_width $PATHC_WIDTH \
+                --gpus $DEVICE \ 
+                --loss $LOSS \ 
+                --epochs $EPOCHS \
+                --batch_size $BATCH_SIZE \ 
+                --max_depth $MAXDEPTH \ 
+                --num_sample $NUM_SAMPLE \ 
+                --top_crop $TOPCROP \ 
+                --left_crop $LEFTCROP
+                --test_crop \ 
+                --save $SAVE \ 
+                --dep_src $DEPSRC \ 
+                --augment True \ 
+                --model_name $MODELNAME \ 
+                --port $PORT \  
+                --input_conf $INPUTCONF
