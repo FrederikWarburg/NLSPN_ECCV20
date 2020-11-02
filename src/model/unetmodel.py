be70e6f3f30b43fb1f912d553dc8b0e5d235fbe6
FrederikWarburg <frewar1905@gmail.com>
2020-11-02 15:14:16 +0000

    rever

--- src/model/unetmodel.py
+++ src/model/unetmodel.py
@@ -1,11 +1,144 @@
 
 
 import torch
-from .common import *
 from .visualtransformer import *
+from .common import get_resnet18, get_resnet34
 from torchvision.models.resnet import BasicBlock
 from torchvision import models
 
+def _guide( fd_rgb, fe_rgb, fe_dep, guide = 'cat', dim = 1):
+
+    if guide == 'cat':
+        return _concat(_concat(fd_rgb, fe_rgb, aggregate='cat', dim=1), fe_dep, aggregate='cat', dim=1)
+    elif guide == 'sum':
+        return _concat(_concat(fd_rgb, fe_rgb, aggregate='sum', dim=1), fe_dep, aggregate='sum', dim=1)
+    elif guide == 'none':
+        return fe_dep
+
+def _concat(fd, fe, aggregate='cat', dim=1):
+    
+    fd = _remove_extra_pad(fd, fe)
+
+    if aggregate == 'cat':
+        f = torch.cat((fd, fe), dim=dim)
+    elif aggregate == 'sum':
+        f = fd + fe
+
+    return f
+
+def _remove_extra_pad(fd, fe, dim=1):
+
+    # Decoder feature may have additional padding
+    _, _, Hd, Wd = fd.shape
+    _, _, He, We = fe.shape
+
+    if abs(Hd - He) > 1 or abs(Wd - We) > 1:
+        print("warning", fd.shape, fe.shape)
+
+    # Remove additional padding
+    if Hd > He:
+        h = Hd - He
+        fd = fd[:, :, :-h, :]
+
+    if Wd > We:
+        w = Wd - We
+        fd = fd[:, :, :, :-w]
+
+    return fd
+
+def _make_layer(inplanes, planes, blocks=1, stride=1):
+    downsample = None
+    block = BasicBlock
+
+    if stride != 1 or inplanes != planes * block.expansion:
+        downsample = torch.nn.Sequential(
+            torch.nn.Conv2d(inplanes, planes * block.expansion,
+                        kernel_size=1, stride=stride, bias=False),
+            torch.nn.BatchNorm2d(planes * block.expansion),
+        )
+
+    layers = []
+    layers.append(block(inplanes, planes, stride, downsample))
+    inplanes = planes * block.expansion
+    for _ in range(1, blocks):
+        layers.append(block(inplanes, planes))
+
+    return torch.nn.Sequential(*layers)
+
+
+def _upsampling(ch_in, ch_out, kernel, stride=1, padding=0, output_padding=0,
+                bn=True, relu=True, upsampling = 'learnable'):
+
+    layers = []
+    if upsampling == 'learnable':
+        layers.append(nn.ConvTranspose2d(ch_in, ch_out, kernel, stride, padding, output_padding))
+    else:
+        layers.append(nn.Upsample(mode='bilinear', scale_factor=2))
+        layers.append(nn.Conv2d(ch_in, ch_out, kernel_size=1, stride=1))
+    
+    if bn:
+        layers.append(nn.BatchNorm2d(ch_out))
+    if relu:
+        layers.append(nn.LeakyReLU(0.2, inplace=True))
+
+    layers = nn.Sequential(*layers)
+
+    return layers
+
+def conv_bn_relu(ch_in, ch_out, kernel, stride=1, padding=0, bn=True,
+                relu=True, maxpool=False):
+    assert (kernel % 2) == 1, \
+        'only odd kernel is supported but kernel = {}'.format(kernel)
+
+    layers = []
+    layers.append(nn.Conv2d(ch_in, ch_out, kernel, stride, padding, bias=not bn))
+    if bn:
+        layers.append(nn.BatchNorm2d(ch_out))
+    if relu:
+        layers.append(nn.LeakyReLU(0.2, inplace=True))
+    if maxpool:
+        layers.append(nn.MaxPool2d(kernel_size=3, stride=2, padding=1, dilation=1, ceil_mode=False))
+
+    layers = nn.Sequential(*layers)
+
+    return layers
+
+
+def double_conv(ch_in, ch_out, kernel, stride=1, padding=0, output_padding=0,
+                bn=True, relu=True, upsampling = 'learnable'):
+
+    layers = []
+
+    layers.append(conv_bn_relu(ch_out, ch_out, kernel=(3,3), padding=(1,1)))
+    layers.append(conv_bn_relu(ch_out, ch_out, kernel=(3,3), padding=(1,1)))
+
+    layers = nn.Sequential(*layers)
+
+    return layers
+
+
+
+class Upsample(nn.Module):
+    def __init__(self, ch_in, ch_out, kernel, stride=1, padding=0, output_padding=0, bn=True, relu=True, upsampling = 'learnable', aggregate = 'cat'):
+        super(Upsample, self).__init__()
+
+
+        self.aggregate = aggregate
+        self.upsampling = _upsampling(ch_in, ch_out, kernel, stride=stride, padding=padding, output_padding=output_padding,
+                bn=bn, relu=relu, upsampling = upsampling)
+        self.conv = double_conv(ch_in, ch_out, kernel, stride=stride, padding=padding, output_padding=output_padding,
+                bn=bn, relu=relu)
+
+    def forward(self, x, x1 = None):
+
+        x = self.upsampling(x)
+
+        if x1 is not None:
+            x = _concat(x, x1, aggregate=self.aggregate, dim=1)
+
+        x = self.conv(x)
+
+        return x
 
 class UNETModel(nn.Module):
     def __init__(self, args = None):
@@ -54,10 +187,10 @@ class UNETModel(nn.Module):
         self.conv6_rgb = self.conv_bn_relu(512, 1024, kernel=3, stride=2, padding=1, bn=True, relu=True) # 1/32
 
         # Decoder
-        self.dec5_rgb = self.convt_bn_relu(1024, 512, kernel=3, stride=2, padding=1, output_padding=1, upsampling=self.upsampling) # 1/16
-        self.dec4_rgb = self.convt_bn_relu(512+self.D_skip * 512, 256, kernel=3, stride=2, padding=1, output_padding=1, upsampling=self.upsampling) # 1/8
-        self.dec3_rgb = self.convt_bn_relu(256+self.D_skip * 256, 128, kernel=3, stride=2, padding=1, output_padding=1, upsampling=self.upsampling) # 1/4
-        self.dec2_rgb = self.convt_bn_relu(128+self.D_skip * 128, 64, kernel=3, stride=2, padding=1, output_padding=1, upsampling=self.upsampling) # 1/2
+        self.dec5_rgb = Upsample(1024, 512, kernel=3, stride=2, padding=1, output_padding=1, upsampling=self.upsampling) # 1/16
+        self.dec4_rgb = Upsample(512+self.D_skip * 512, 256, kernel=3, stride=2, padding=1, output_padding=1, upsampling=self.upsampling) # 1/8
+        self.dec3_rgb = Upsample(256+self.D_skip * 256, 128, kernel=3, stride=2, padding=1, output_padding=1, upsampling=self.upsampling) # 1/4
+        self.dec2_rgb = Upsample(128+self.D_skip * 128, 64, kernel=3, stride=2, padding=1, output_padding=1, upsampling=self.upsampling) # 1/2
         self.dec1_rgb = self.conv_bn_relu(64+self.D_skip * 64, 64, kernel=3, stride=1, padding=1) # 1/2
 
         ####
@@ -79,11 +212,11 @@ class UNETModel(nn.Module):
         self.conv6_dep = self.conv_bn_relu(512, 1024, kernel=3, stride=2, padding=1, bn=True, relu=True, maxpool=False) # 1/32
 
         # Decoder
-        self.dec5_dep = self.convt_bn_relu(1024, 512, kernel=3, stride=2, padding=1, output_padding=1, upsampling=self.upsampling) # 1/16
-        self.dec4_dep = self.convt_bn_relu(512+self.D_skip * 512, 256, kernel=3, stride=2, padding=1, output_padding=1, upsampling=self.upsampling) # 1/8
-        self.dec3_dep = self.convt_bn_relu(256+self.D_skip * 256, 128, kernel=3, stride=2, padding=1, output_padding=1, upsampling=self.upsampling) # 1/4
-        self.dec2_dep = self.convt_bn_relu(128+self.D_skip * 128, 64, kernel=3, stride=2, padding=1, output_padding=1, upsampling=self.upsampling) # 1/2
-        self.dec1_dep = self.convt_bn_relu(64+self.D_skip * 64, 64, kernel=3, stride=2, padding=1, output_padding=1, upsampling=self.upsampling) # 1/1
+        self.dec5_dep = Upsample(1024, 512, kernel=3, stride=2, padding=1, output_padding=1, upsampling=self.upsampling) # 1/16
+        self.dec4_dep = Upsample(512+self.D_skip * 512, 256, kernel=3, stride=2, padding=1, output_padding=1, upsampling=self.upsampling) # 1/8
+        self.dec3_dep = Upsample(256+self.D_skip * 256, 128, kernel=3, stride=2, padding=1, output_padding=1, upsampling=self.upsampling) # 1/4
+        self.dec2_dep = Upsample(128+self.D_skip * 128, 64, kernel=3, stride=2, padding=1, output_padding=1, upsampling=self.upsampling) # 1/2
+        self.dec1_dep = Upsample(64+self.D_skip * 64, 64, kernel=3, stride=2, padding=1, output_padding=1, upsampling=self.upsampling) # 1/1
 
         # Depth Branch
         self.id_dec1 = self.conv_bn_relu(64, 64, kernel=3, stride=1, padding=1, bn=False, relu=True) # 1/1
@@ -107,112 +240,6 @@ class UNETModel(nn.Module):
             self.vt4 = VisualTransformer(L=args.num_tokens, CT=args.token_size, C=512, size = 64, num_downsample = 3,head=args.num_heads, groups=args.groups, kqv_groups=args.kqv_groups, dynamic=False)
 
 
-    def _make_layer(self, inplanes, planes, blocks=1, stride=1):
-        downsample = None
-        block = BasicBlock
-
-        if stride != 1 or inplanes != planes * block.expansion:
-            downsample = torch.nn.Sequential(
-                torch.nn.Conv2d(inplanes, planes * block.expansion,
-                          kernel_size=1, stride=stride, bias=False),
-                torch.nn.BatchNorm2d(planes * block.expansion),
-            )
-
-        layers = []
-        layers.append(block(inplanes, planes, stride, downsample))
-        inplanes = planes * block.expansion
-        for _ in range(1, blocks):
-            layers.append(block(inplanes, planes))
-
-        return torch.nn.Sequential(*layers)
-
-    def convt_bn_relu(self, ch_in, ch_out, kernel, stride=1, padding=0, output_padding=0,
-                  bn=True, relu=True, upsampling = 'learnable'):
-        assert (kernel % 2) == 1, \
-            'only odd kernel is supported but kernel = {}'.format(kernel)
-
-        layers = []
-        if upsampling == 'learnable':
-            layers.append(nn.ConvTranspose2d(ch_in, ch_out, kernel, stride, padding, output_padding))
-        else:
-            layers.append(nn.Upsample(mode='bilinear', scale_factor=2))
-            layers.append(nn.Conv2d(ch_in, ch_out, kernel_size=1, stride=1))
-        
-        if bn:
-            layers.append(nn.BatchNorm2d(ch_out))
-        if relu:
-            layers.append(nn.LeakyReLU(0.2, inplace=True))
-
-        layers.append(nn.Conv2d(ch_out, ch_out, kernel_size=(3,3), padding=(1,1)))
-        layers.append(nn.BatchNorm2d(ch_out))
-        layers.append(nn.LeakyReLU(0.2, inplace=True))
-        layers.append(nn.Conv2d(ch_out, ch_out, kernel_size=(3,3), padding=(1,1)))
-        layers.append(nn.BatchNorm2d(ch_out))
-        layers.append(nn.LeakyReLU(0.2, inplace=True))
-
-        layers = nn.Sequential(*layers)
-
-        return layers
-
-    def conv_bn_relu(self, ch_in, ch_out, kernel, stride=1, padding=0, bn=True,
-                 relu=True, maxpool=False):
-        assert (kernel % 2) == 1, \
-            'only odd kernel is supported but kernel = {}'.format(kernel)
-
-        layers = []
-        layers.append(nn.Conv2d(ch_in, ch_out, kernel, stride, padding, bias=not bn))
-        if bn:
-            layers.append(nn.BatchNorm2d(ch_out))
-        if relu:
-            layers.append(nn.LeakyReLU(0.2, inplace=True))
-        if maxpool:
-            layers.append(nn.MaxPool2d(kernel_size=3, stride=2, padding=1, dilation=1, ceil_mode=False))
-
-        layers = nn.Sequential(*layers)
-
-        return layers
-
-
-    def _guide(self, fd_rgb, fe_rgb, fe_dep, guide = 'cat', dim = 1):
-
-        if guide == 'cat':
-            return self._concat(self._concat(fd_rgb, fe_rgb, aggregate='cat', dim=1), fe_dep, aggregate='cat', dim=1)
-        elif guide == 'sum':
-            return self._concat(self._concat(fd_rgb, fe_rgb, aggregate='sum', dim=1), fe_dep, aggregate='sum', dim=1)
-        elif guide == 'none':
-            return fe_dep
-
-    def _concat(self, fd, fe, aggregate='cat', dim=1):
-        
-        fd = self._remove_extra_pad(fd, fe)
-
-        if aggregate == 'cat':
-            f = torch.cat((fd, fe), dim=dim)
-        elif aggregate == 'sum':
-            f = fd + fe
-
-        return f
-
-    def _remove_extra_pad(self, fd, fe, dim=1):
-
-        # Decoder feature may have additional padding
-        _, _, Hd, Wd = fd.shape
-        _, _, He, We = fe.shape
-
-        if abs(Hd - He) > 1 or abs(Wd - We) > 1:
-            print("warning", fd.shape, fe.shape)
-
-        # Remove additional padding
-        if Hd > He:
-            h = Hd - He
-            fd = fd[:, :, :-h, :]
-
-        if Wd > We:
-            w = Wd - We
-            fd = fd[:, :, :, :-w]
-
-        return fd
-
     def forward(self, sample):
 
         rgb = sample['rgb']
@@ -228,10 +255,10 @@ class UNETModel(nn.Module):
 
         # Decoding RGB
         fd5_rgb = self.dec5_rgb(fe6_rgb)
-        fd4_rgb = self.dec4_rgb(self._concat(fd5_rgb, fe5_rgb, aggregate=self.aggregate, dim=1))
-        fd3_rgb = self.dec3_rgb(self._concat(fd4_rgb, fe4_rgb, aggregate=self.aggregate, dim=1))
-        fd2_rgb = self.dec2_rgb(self._concat(fd3_rgb, fe3_rgb, aggregate=self.aggregate, dim=1))
-        fd1_rgb = self.dec1_rgb(self._concat(fd2_rgb, fe2_rgb, aggregate=self.aggregate, dim=1))
+        fd4_rgb = self.dec4_rgb(fd5_rgb, fe5_rgb)
+        fd3_rgb = self.dec3_rgb(fd4_rgb, fe4_rgb)
+        fd2_rgb = self.dec2_rgb(fd3_rgb, fe3_rgb)
+        fd1_rgb = self.dec1_rgb(fd2_rgb, fe2_rgb)
         
         ###
         # DEPTH UNET
@@ -239,42 +266,42 @@ class UNETModel(nn.Module):
         
         # Encoding Depth
         fe1_dep = self.conv1_dep(dep)
-        fe2_dep = self.conv2_dep(self._guide(fd1_rgb, fe1_rgb, fe1_dep, guide=self.guide, dim=1))
+        fe2_dep = self.conv2_dep(_guide(fd1_rgb, fe1_rgb, fe1_dep, guide=self.guide, dim=1))
 
         if self.args.attention_type == 'VT':
             # we need first to remove some extra padding which is added in the decoding stage
-            fd2_rgb = self._remove_extra_pad(fd2_rgb, fe2_dep)
+            fd2_rgb = _remove_extra_pad(fd2_rgb, fe2_dep)
             fe2_dep = self.vt1(fd2_rgb, fe2_dep)
 
-        fe3_dep = self.conv3_dep(self._guide(fd2_rgb, fe2_rgb, fe2_dep, guide=self.guide, dim=1))
+        fe3_dep = self.conv3_dep(_guide(fd2_rgb, fe2_rgb, fe2_dep, guide=self.guide, dim=1))
 
         if self.args.attention_type == 'VT':
             # we need first to remove some extra padding which is added in the decoding stage
-            fd3_rgb = self._remove_extra_pad(fd3_rgb, fe3_dep)
+            fd3_rgb = _remove_extra_pad(fd3_rgb, fe3_dep)
             fe3_dep = self.vt2(fd3_rgb, fe3_dep)
 
-        fe4_dep = self.conv4_dep(self._guide(fd3_rgb, fe3_rgb, fe3_dep, guide=self.guide, dim=1))
+        fe4_dep = self.conv4_dep(_guide(fd3_rgb, fe3_rgb, fe3_dep, guide=self.guide, dim=1))
 
         if self.args.attention_type == 'VT':
             # we need first to remove some extra padding which is added in the decoding stage
-            fd4_rgb = self._remove_extra_pad(fd4_rgb, fe4_dep)
+            fd4_rgb = _remove_extra_pad(fd4_rgb, fe4_dep)
             fe4_dep = self.vt3(fd4_rgb, fe4_dep)
 
-        fe5_dep = self.conv5_dep(self._guide(fd4_rgb, fe4_rgb, fe4_dep, guide=self.guide, dim=1))
+        fe5_dep = self.conv5_dep(_guide(fd4_rgb, fe4_rgb, fe4_dep, guide=self.guide, dim=1))
 
         if self.args.attention_type == 'VT':
             # we need first to remove some extra padding which is added in the decoding stage
-            fd5_rgb = self._remove_extra_pad(fd5_rgb, fe5_dep)
+            fd5_rgb = _remove_extra_pad(fd5_rgb, fe5_dep)
             fe5_dep = self.vt4(fd5_rgb, fe5_dep)
 
         fe6_dep = self.conv6_dep(fe5_dep)
         
         # Decoding Depth
         fd5_dep = self.dec5_dep(fe6_dep)
-        fd4_dep = self.dec4_dep(self._concat(fd5_dep, fe5_dep, aggregate=self.aggregate, dim=1))
-        fd3_dep = self.dec3_dep(self._concat(fd4_dep, fe4_dep, aggregate=self.aggregate, dim=1))
-        fd2_dep = self.dec2_dep(self._concat(fd3_dep, fe3_dep, aggregate=self.aggregate, dim=1))   
-        fd1_dep = self.dec1_dep(self._concat(fd2_dep, fe2_dep, aggregate=self.aggregate, dim=1))
+        fd4_dep = self.dec4_dep(fd5_dep, fe5_dep)
+        fd3_dep = self.dec3_dep(fd4_dep, fe4_dep)
+        fd2_dep = self.dec2_dep(fd3_dep, fe3_dep)   
+        fd1_dep = self.dec1_dep(fd2_dep, fe2_dep)
 
         ###
         # PREDICTION HEADS
@@ -288,8 +315,8 @@ class UNETModel(nn.Module):
         cf_fd1 = self.cf_dec1(fd1_dep)
         confidence = self.cf_dec0(cf_fd1)
 
-        pred = self._remove_extra_pad(pred, dep)
-        confidence = self._remove_extra_pad(confidence, dep)
+        pred = _remove_extra_pad(pred, dep)
+        confidence = _remove_extra_pad(confidence, dep)
 
         if self.args.attention_type == 'VT':
             output = {'pred': pred, 'confidence': confidence, 'vt1': self.vt1, 'vt2': self.vt2, 'vt3':self.vt3, 'vt4':self.vt4}
